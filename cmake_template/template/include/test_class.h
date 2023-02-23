7e3d715bc7265285e86ec6fdbcc2ca2e9dc83cc1
汪浩 <wanghao@zonesion.comc.cn>
2023-02-23 17:24:49 +0800

    gx

---
+++ cmake_template/template/include/test_class.h
@@ -0,0 +1,17 @@
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+#include<iostream>
+#include<stdio.h>
+ 
+class ValueClass
+{
+ 
+private:
+    int value;
+    int sum;
+public:
+    ValueClass();
+    void Add(int i, int j);
+};
+ 
+#endif 
